488b08755a47ef2c0fdd97ec3f40fc5879327650
Alexsandr <1inboxna@gmail.com>
2024-12-02 21:50:48 +0200

    start

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+import Header from './components/header/header'
+
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import "./header.css"
+
+function Header() {
+    return (
+        <>
+            <h1>Burger menu</h1>
+        </>
+    )
+  
+}
+
+export default Header
